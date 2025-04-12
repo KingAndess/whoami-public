9d4bf24b8ce0b86de6c0418ac042d62b15fcf25b
KingAndess <andishirat@gmail.com>
2025-04-12 13:33:09 +0800

    print whoami readme

---
+++ whoami.js
@@ -0,0 +1,8 @@
+const name = "Andi Shirat Maqbul";
+const instagram = "@andishirat_";
+const age = 21;
+
+console.log("Who Am I");
+console.log("Nama:", name);
+console.log("Instagram:", instagram);
+console.log("Age:", age);
